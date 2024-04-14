ca4c836736741daf70a54e882a19efe037d03210
Jair Schwan <jair.schwan9@gmail.com>
2024-04-14 19:26:10 -0300

    first commit

---
+++ src/main/java/com/fiapfastfood/groupfoursix/GroupfoursixApplication.java
@@ -0,0 +1,13 @@
+package com.fiapfastfood.groupfoursix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupfoursixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupfoursixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiapfastfood/groupfoursix/GroupforsixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiapfastfood.groupfoursix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupforsixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
