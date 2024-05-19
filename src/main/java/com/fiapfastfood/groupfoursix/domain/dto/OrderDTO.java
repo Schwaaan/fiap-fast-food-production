319fb4e3f270fe900c4f1e18089a7bbdc60154ea
Jair Schwan <jair.schwan9@gmail.com>
2024-05-19 19:54:29 -0300

    only add env lombok use

--- src/main/java/com/fiapfastfood/groupfoursix/domain/dto/OrderDTO.java
+++ src/main/java/com/fiapfastfood/groupfoursix/domain/dto/OrderDTO.java
@@ -1,8 +1,10 @@
 package com.fiapfastfood.groupfoursix.domain.dto;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 public class OrderDTO {
 
     private String id;
