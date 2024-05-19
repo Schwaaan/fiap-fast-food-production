cd6b792ff811bee5ef9ae2551e8abf654b2712cd
Jair Schwan <jair.schwan9@gmail.com>
2024-05-19 19:55:13 -0300

    only add env lombok use

--- src/main/java/com/fiapfastfood/groupfoursix/domain/Order.java
+++ src/main/java/com/fiapfastfood/groupfoursix/domain/Order.java
@@ -1,11 +1,13 @@
 package com.fiapfastfood.groupfoursix.domain;
 
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Data
+@Getter
+@Setter
 @Document
 public class Order {
 
