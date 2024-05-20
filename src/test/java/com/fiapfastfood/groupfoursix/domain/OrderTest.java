c13e7214641540ae46bdc9f552a8c84a30ae9717
Jair Schwan <jair.schwan9@gmail.com>
2024-05-19 21:07:06 -0300

    add more tests

--- src/test/java/com/fiapfastfood/groupfoursix/domain/OrderTest.java
+++ src/test/java/com/fiapfastfood/groupfoursix/domain/OrderTest.java
@@ -45,4 +45,37 @@ class OrderTest {
         assertEquals("1", order.getId());
         assertEquals(OrderStatus.PROGRESS, order.getOrderStatus());
     }
+
+    @Test
+    public void testHashCode() {
+        Order order1 = new Order("1", OrderStatus.PROGRESS);
+        Order order2 = new Order("1", OrderStatus.PROGRESS);
+        assertEquals(order1.hashCode(), order2.hashCode());
+    }
+
+    @Test
+    public void testEquals() {
+        Order order1 = new Order("1", OrderStatus.PROGRESS);
+        Order order2 = new Order("1", OrderStatus.PROGRESS);
+        Order order3 = new Order("2", OrderStatus.PROGRESS);
+
+        assertTrue(order1.equals(order2));
+        assertFalse(order1.equals(order3));
+    }
+
+    @Test
+    public void testCanEqual() {
+        Order order1 = new Order("1", OrderStatus.PROGRESS);
+        Order order2 = new Order("1", OrderStatus.PROGRESS);
+
+        assertTrue(order1.canEqual(order2));
+    }
+
+    @Test
+    public void testToString() {
+        Order order = new Order("1", OrderStatus.PROGRESS);
+
+        String expectedToString = "Order(id=1, orderStatus=PROGRESS)";
+        assertEquals(expectedToString, order.toString());
+    }
 }
